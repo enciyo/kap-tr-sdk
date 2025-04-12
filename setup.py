5dd26299e9ab10f262f4a7201b50249fbea3465d
enciyo <enciyomk61@gmail.com>
2025-04-12 14:51:15 +0300

    Fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='kap_sdk',
-    version='0.1.0',
+    version='0.1.1',
     description='Kap Data Scrapping',
     packages=find_packages(),
     install_requires=[
